c1630fcd9aca9d301922f1bf714805a7927bd4c6
smart 862 <91789414+smarttechy862@users.noreply.github.com>
2022-10-08 14:38:48 +0530

    Update leapyear.kt

--- leapyear.kt
+++ leapyear.kt
@@ -14,3 +14,22 @@ fun main(args: Array<String>) {
 
     println(if (leap) "$year is a leap year." else "$year is not a leap year.")
 }
+
+Kotlin Program to Check Leap Year using when expression
+fun main(args: Array<String>) {
+
+    val year = 2012
+    var leap = false
+
+    leap = when {
+        year % 4 == 0 -> {
+            when {
+                year % 100 == 0 -> year % 400 == 0
+                else -> true
+            }
+        }
+        else -> false
+    }
+
+    println(if (leap) "$year is a leap year." else "$year is not a leap year.")
+}
